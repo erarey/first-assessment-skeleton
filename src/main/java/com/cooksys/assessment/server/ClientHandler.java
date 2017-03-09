9930b1eaa9f73d2c5e3f30b91002fe54b4bcf300
Eli Rarey <erarey@gmail.com>
2017-03-09 03:48:56 -0600

    Broadcast implemented
    Prevented the printWriter from blocking execution of clientHandler thread.
    Temporarily moved important logic into new MessageCenter.class, but ultimately plan to relocate it back to Server now that other major problems getting broadcast to work have been resolved.

--- src/main/java/com/cooksys/assessment/server/ClientHandler.java
+++ src/main/java/com/cooksys/assessment/server/ClientHandler.java
@@ -9,11 +9,13 @@ import java.net.Socket;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.cooksys.assessment.model.Message;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class ClientHandler implements Runnable {
@@ -23,16 +25,21 @@ public class ClientHandler implements Runnable {
 
 	private MessageCenter messageCenter = null;
 
+	boolean alive = true;
+
 	Queue<Message> unreadMessages = new LinkedList<Message>();
 
 	Queue<Message> unsentMessages = new LinkedList<Message>();
 
 	String thisUsername = "";
 
+	boolean hasBeenAnnounced = false;
+
 	public ClientHandler(Socket socket, MessageCenter messageCenter) {
 		super();
 		this.socket = socket;
 		this.messageCenter = messageCenter;
+		messageCenter.addClientHandler(this);
 	}
 
 	public void run() {
@@ -43,51 +50,48 @@ public class ClientHandler implements Runnable {
 			PrintWriter writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
 
 			while (!socket.isClosed()) {
-				String raw = reader.readLine();
-				Message message = mapper.readValue(raw, Message.class);
+				if (reader.ready()) {
+					String raw = reader.readLine();
+					Message message = mapper.readValue(raw, Message.class);
 
-				switch (message.getCommand()) {
-				case "connect":
-					log.info("user <{}> connected", message.getUsername());
-					thisUsername = message.getUsername();
-					break;
-				case "disconnect":
-					log.info("user <{}> disconnected", message.getUsername());
-					this.socket.close();
-					break;
-				case "echo":
-					log.info("user <{}> echoed message <{}>", message.getUsername(), message.getContents());
-					String response = mapper.writeValueAsString(message);
-					writer.write(response);
-					writer.flush();
-					break;
-				case "broadcast":
-					log.info("user <{}> broadcasted message <{}>", message.getUsername(), message.getContents());
-					// String response2 = mapper.writeValueAsString(message);
-					// writer.write(response2);
-					// writer.flush();
-					unsentMessages.add(message);
-					break;
-				case "whisper":
-					log.info("user <{}> whispered <{}>", message.getUsername(), message.getContents());
-					// String response2 = mapper.writeValueAsString(message);
-					// writer.write(response2);
-					// writer.flush();
-					unsentMessages.add(message);
-					break;
-				}
+					switch (message.getCommand()) {
+					case "connect":
+						log.info("user <{}> connected", message.getUsername());
+						thisUsername = message.getUsername();
+						break;
+					case "disconnect":
+						log.info("user <{}> disconnected", message.getUsername());
+						this.socket.close();
+						break;
+					case "echo":
+						log.info("user <{}> echoed message <{}>", message.getUsername(), message.getContents());
+						String response = mapper.writeValueAsString(message);
+						writer.write(response);
+						writer.flush();
+						break;
+					case "broadcast":
+						log.info("user <{}> broadcasted message <{}>", message.getUsername(), message.getContents());
+						// String response2 =
+						// mapper.writeValueAsString(message);
+						// writer.write(response2);
+						// writer.flush();
+						unsentMessages.add(message);
+						break;
+					case "whisper":
+						log.info("user <{}> whispered <{}>", message.getUsername(), message.getContents());
+						// String response2 =
+						// mapper.writeValueAsString(message);
+						// writer.write(response2);
+						// writer.flush();
+						unsentMessages.add(message);
+						break;
+					}
+				} else {
+					while (!unsentMessages.isEmpty()) {
+						messageCenter.passMessageToMessageCenter(unsentMessages.remove());
+					}
 
-				while (!unsentMessages.isEmpty()) {
-					messageCenter.passMessageToMessageCenter(unsentMessages.remove());
-				}
-				
-				log.debug("unreadMessages B: " + (new Integer(unreadMessages.size()).toString()));
-				
-				while (!unreadMessages.isEmpty()) {
-					log.debug("trying to send from within clienthandler");
-					String response3 = mapper.writeValueAsString(unreadMessages.remove());
-					writer.write(response3);
-					writer.flush();
+					displayMessages(writer, mapper);
 				}
 			}
 
@@ -96,11 +100,28 @@ public class ClientHandler implements Runnable {
 		}
 	}
 
-	public void addMessage(Message message) {
+	public synchronized void addMessage(Message message) {
 		log.debug("adding a message at client handler");
 		unreadMessages.add(message);
 		log.debug("unreadMessages: " + (new Integer(unreadMessages.size()).toString()));
 
+	}
+
+	public synchronized void displayMessages(PrintWriter writer, ObjectMapper mapper) {
+		//log.debug("unreadMessages B: " + (new Integer(unreadMessages.size()).toString()));
+
+		while (!unreadMessages.isEmpty()) {
+			log.debug("trying to send from within clienthandler");
+			String response3;
+			try {
+				response3 = mapper.writeValueAsString(unreadMessages.remove());
+				writer.write(response3);
+				writer.flush();
+			} catch (JsonProcessingException e) {
+				e.printStackTrace();
+			}
+
+		}
 	}
 
 }
--- src/main/java/com/cooksys/assessment/server/MessageCenter.java
+++ src/main/java/com/cooksys/assessment/server/MessageCenter.java
@@ -1,7 +1,10 @@
 package com.cooksys.assessment.server;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
 import com.cooksys.assessment.model.Message;
 
@@ -11,7 +14,9 @@ public class MessageCenter implements Runnable {
 
 	private List<ClientHandler> clientsSyncd = null;
 
-	volatile private List<Message> messagesTemp = new ArrayList<>();
+	private List<ClientHandler> clientsNew = new ArrayList<ClientHandler>();
+	
+	//private List<Message> messagesTemp = new ArrayList<>();
 
 	private List<Message> messages = new ArrayList<>();
 
@@ -19,16 +24,67 @@ public class MessageCenter implements Runnable {
 
 	public MessageCenter(Server server) {
 		this.server = server;
-
+		clientsSyncd = Collections.synchronizedList(clients);
 	}
 
 	@Override
 	public void run() {
-
+		while (true)
+		{
+			try {
+				Thread.sleep(100);
+				pushMessagestoClients();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+			
+		}
 	}
 
 	public synchronized void passMessageToMessageCenter(Message message) {
+		System.out.println("message center got a message");
+		messages.add(message);
 		
+	}
+
+	public synchronized void addClientHandler(ClientHandler clientHandler) {
+		//System.out.println("Before add: " + clientsSyncd.size());
+		
+		clientsSyncd.add(clientHandler);
+		
+		//System.out.println("After add: " + clientsSyncd.size());
+		
+		//List<ClientHandler> clientsTemp = new ArrayList<ClientHandler>();
+		
+		//System.out.println("In temp: " + clientsTemp.size());
+		
+		clientsNew.add(clientHandler);
+	}
+	
+	public synchronized void pushMessagestoClients()
+	{
+		//List<ClientHandler> clientsTemp = new ArrayList<ClientHandler>();
+		//clientsTemp.addAll(clientsSyncd);
+		
+		//ListIterator<ClientHandler> cl = clientsTemp.listIterator();
+		//while (cl.hasNext())
+		for (ClientHandler cl : clientsSyncd)
+		{
+			for (Message msg : messages)
+			{
+				/*if (cl.thisUsername != "" && cl.hasBeenAnnounced == false)
+				{
+					Message msgNew = new Message();
+					msgNew.setUsername("SERVER SAYS "); msgNew.setContents(cl.thisUsername + " has arrived!");
+					cl.hasBeenAnnounced = true;
+				}*/
+				
+				cl.addMessage(msg); //may need to create new message, copy old, send
+			}
+		}
+		//System.out.println("messages before clear " + messages.size());
+		messages.clear();
 		
 	}
 
